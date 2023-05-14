33cfc143dfcf30ae489ebe1e82aa4e69a7e16bee
ctillyer <84575305+ctillyer@users.noreply.github.com>
2023-05-14 19:17:54 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        til#111 on discord. @tillyer on discord>
+        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
       </footer>
     </>
   )
