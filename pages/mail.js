2d7c6ca6e65a6a77820ea58c1ef89ec53fcbeb3c
ctillyer <84575305+ctillyer@users.noreply.github.com>
2023-05-14 20:09:23 +0100

    Delete mail.js

--- pages/mail.js
+++
@@ -1,6 +0,0 @@
- <form method="post" action="2013.php" onsubmit="return CheckForBlank();">
-        Name: <input type="text" name="name" id="name"/>
-        Age: <input type="text" name="age" id="age"/>
-        Email: <input type="text" name="email" id="email"/>
-        <p><input type="submit" value="Submit" /></p>
-    </form>
