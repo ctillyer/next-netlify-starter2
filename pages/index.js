ac1137bf42a05e60d68288256bc6cf916051d7db
ctillyer <84575305+ctillyer@users.noreply.github.com>
2023-05-18 21:01:10 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,40 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Footer from '../components/Footer'
+import { useState, useEffect } from 'react';
+
+const lyric1 = ["purify my soul", "losing all control"];
+const lyric2 = ["- wash away my sins -", "- that's how it all begins -"];
 
 export default function Home() {
+
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const tick = () => setIndex(i => i + 1);
+    const id = setInterval(tick, 4000);
+    return () => clearInterval(id);
+  }, []);
+
+  const [fadeProp, setFadeProp] = useState({
+    fade:'fade-in'
+  })
+
+  useEffect(() => {
+    const timeout = setInterval(() => {
+       if (fadeProp.fade === 'fade-in') {
+          setFadeProp({
+               fade: 'fade-out'
+          })
+       } else {
+            setFadeProp({
+               fade: 'fade-in'
+            })
+       }
+    }, 2000);
+    
+return () => clearInterval(timeout)
+}, [fadeProp])
+
   return (
     <div className="container">
       <Head>
@@ -10,10 +42,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+
       <main>
-        <Header title="purify my soul" />
-        <p className="description">
-          - wash away my sins -
+        <h1 className={fadeProp.fade}>{lyric1[index % lyric1.length]}</h1>
+
+        <p className={fadeProp.fade}>
+        {lyric2[index % lyric2.length]}
         </p>
       </main>
 
